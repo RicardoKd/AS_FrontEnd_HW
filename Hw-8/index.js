66b5ad6d7e453309b9b9f179fc6dc2bc732f7492
RicardoKd <richardo.kadyan@gmail.com>
2020-06-01 16:03:06 +0300

    Hw 8 2.0

--- Hw-8/index.js
+++ Hw-8/index.js
@@ -10,56 +10,20 @@ async function loadJSON(url) {
 }
 
 const loadToScreen = (json) => {
-  const container = document.createElement("div");
-  container.classList.add("container");
-
-  const titleContainer = document.createElement("div");
-  const directorContainer = document.createElement("div");
-  const releaseDateContainer = document.createElement("div");
-
-  const titleContainerName = document.createElement("p");
-  const directorContainerName = document.createElement("p");
-  const releaseDateContainerName = document.createElement("p");
-
-  titleContainerName.classList.add("ulTitle");
-  directorContainerName.classList.add("ulTitle");
-  releaseDateContainerName.classList.add("ulTitle");
-
-  titleContainerName.innerText = "Film Title";
-  directorContainerName.innerText = "Director";
-  releaseDateContainerName.innerText = "Release Date";
-
-  const TitleUl = document.createElement("ul");
-  const DirectorUl = document.createElement("ul");
-  const releaseDateUl = document.createElement("ul");
-
+  let titleLi = "";
+  let directorLi = "";
+  let releaseDateLi = "";
   json.forEach((elem) => {
-    const titleLi = document.createElement("li");
-    const DirectorLi = document.createElement("li");
-    const releaseDateLi = document.createElement("li");
-
-    titleLi.innerText = elem.title;
-    TitleUl.append(titleLi);
-
-    DirectorLi.innerText = elem.director;
-    DirectorUl.append(DirectorLi);
-
-    releaseDateLi.innerText = elem.release_date;
-    releaseDateUl.append(releaseDateLi);
+    titleLi += `<li>${elem.title}</li>`;
+    directorLi += `<li>${elem.director}</li>`;
+    releaseDateLi += `<li>${elem.release_date}</li>`;
   });
 
-  container.append(titleContainer);
-  container.append(directorContainer);
-  container.append(releaseDateContainer);
+  const titleDivHTML = `<div><p class='ulTitle'>Film Title</p><ul>${titleLi}</ul></div>`;
+  const directorDivHTML = `<div><p class='ulTitle'>Director</p><ul>${directorLi}</ul></div>`;
+  const releaseDateHTML = `<div><p class='ulTitle'>Release Date</p><ul>${releaseDateLi}</ul></div>`;
 
-  titleContainer.append(titleContainerName);
-  directorContainer.append(directorContainerName);
-  releaseDateContainer.append(releaseDateContainerName);
-  titleContainer.append(TitleUl);
-  directorContainer.append(DirectorUl);
-  releaseDateContainer.append(releaseDateUl);
-
-  document.body.append(container);
+  document.body.innerHTML += `<div class="container">${titleDivHTML}${directorDivHTML}${releaseDateHTML}</div>`;
 };
 
 loadJSON("https://swapi.dev/api/films").then(
